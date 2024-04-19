e44c5b1c5ce5c8a6f028f01a496bdaa87003de5d
er3nnz <er3naksu@gmail.com>
2024-04-19 20:54:37 +0300

    Chat App Project

---
+++ src/main/java/dev/io/chatapp/chat/ChatMessage.java
@@ -0,0 +1,25 @@
+package dev.io.chatapp.chat;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+    private MessageType type;
+    private String content;
+    private String sender;
+
+    public enum MessageType {
+        CHAT,
+        JOIN,
+        LEAVE
+    }
+
+    public enum MessageStatus {
+        SENT,
+        RECEIVED
+    }
+}
